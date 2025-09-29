568b92832a2be06d5b21eb9a451e9b6a8d8d4c81
Gustavo Sanches <gustavoboo12345@outlook.com>
2025-09-29 14:18:51 -0300

    Initial commit

---
+++ src/main/java/com/gsanches/file_format_converter/FileFormatConverterApplication.java
@@ -0,0 +1,13 @@
+package com.gsanches.file_format_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileFormatConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileFormatConverterApplication.class, args);
+	}
+
+}
